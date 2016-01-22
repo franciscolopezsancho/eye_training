b503d8c90e2157473b76c6e83d90d5495e99c449
franciscolopezsancho <franciscolopezsancho@gmail.com>
2016-01-22 13:39:18 +0000

    all but one test passing...gonna try to fix trim

---
+++ EyeShake/jasmine/spec/FastReaderSpec.js
@@ -0,0 +1,131 @@
+describe("FastReader", function() {
+  
+
+  beforeEach(function() {
+     reader = new FastReader();
+  });
+
+  it("1 should be able to take 3 words from the begining of 'should be able to play a Song', adding a blank ", function() {
+    expect("should be able ").toEqual(reader.take_text("should be able to play a Song",3,true));
+  });
+  
+  it("2 should be able to move 3 first words from 'should be able to play a Song' to 'almos nothing',adding it before and keep the rest", function() {
+    expect("should be able almos nothing").toEqual(reader.move_text("should be able to play a Song","almos nothing",3,true,true));
+  });
+  
+  it("3 should be able to move 'play a Song' words from 'should be able to play a Song' to 'almos nothing',blank included", function() {
+    expect("almos nothing play a Song").toEqual(reader.move_text("should be able to play a Song","almos nothing",3,false,false));
+  });
+  
+  it("4 should be able to take 3 words from the end of 'should be able to play a Song',blank included", function() {
+    expect(" play a Song").toEqual(reader.take_text("should be able to play a Song",3,false));
+  });
+  
+  it("5 should be able to find index of 3rd blank from the end of 'should be able to play a Song',blank included", function() {
+    expect(18).toEqual(reader.lastIndexOfRec("should be able to play a Song",3," "));
+  });
+  
+  // it("should be able to redistribute", function() {
+ // var html = $('<div class="book"></div>')
+ // 	  				html.append('<div id="upper-container" class="upper">')
+ // 					html.append(' <span id="text-bucket-init" class="text-upper">The story of human kind</span>')
+ // 					html.append('</div>')
+ // 					html.append('<div id="text-bucket-styled" class="reader">Im looking at</div>')
+ // 					html.append('<div id="text-bucket-end" class="downer"></div>')
+ // 	  				html.append('</div>')
+ // 	  var from = $("#text-bucket-styled",html);
+ // 	  var to = $("#text-bucket-init",html);
+ // 	  reader.goingUp(from,to,3)
+ //    expect("The story of human kind Im looking at").toEqual(to.text());
+ //  });
+ it("6 should be able to redistribute 3 words up with nothing in upper container and 'Im looking at' in center", function() {
+var html = $('<div class="book">')		
+  				html.append('<div id="upper-container" class="upper"></div>')		
+					html.append('<div id="text-bucket-styled" class="reader"><p>Im looking at</p></div>')	
+					html.append('<div id="text-bucket-end" class="downer"></div>')	
+	  				html.append('</div>')
+  var from = $("#text-bucket-styled",html);
+  var to = $("#upper-container",html);
+  reader.redistributing_up(from,to,3)
+   expect("Im looking at").toEqual(to.children().last().text());
+ });
+  it("7 should be able to redistribute with some <p> in upper container without adding one blank", function() {
+ var html = $('<div class="book">')		
+	  				html.append('<div id="upper-container" class="upper"><p>Something</p></div>')		
+ 					html.append('<div id="text-bucket-styled" class="reader"><p>Im looking at</p></div>')	
+ 					html.append('<div id="text-bucket-end" class="downer"></div>')	
+ 	  				html.append('</div>')
+	  var from = $("#text-bucket-styled",html);
+	  var to = $("#upper-container",html);
+	  reader.redistributing_up(from,to,3)
+    expect("Something Im looking at").toEqual(to.children().last().text());
+  });
+  it("8 should be able to redistribute to upper with 'Something' and center 'Im looking at' and clean center", function() {
+ var html = $('<div class="book">')		
+	  				html.append('<div id="upper-container" class="upper"><p>Something</p></div>')		
+ 					html.append('<div id="text-bucket-styled" class="reader"><p>Im looking at</p></div>')	
+ 					html.append('<div id="text-bucket-end" class="downer"></div>')	
+ 	  				html.append('</div>')
+	  var from = $("#text-bucket-styled",html);
+	  var to = $("#upper-container",html);
+	  reader.redistributing_up(from,to,3)
+    expect("Something Im looking at").toEqual(to.children().last().text());
+    expect("").toEqual(from.text());
+	
+  });
+  
+  it("9 should be able to redistribute from bottom with 'Something dude was almost walking...' to reader with 'Im looking at'", function() {
+ var html = $('<div class="book">')		
+	  				html.append('<div id="upper-container" class="upper"><p>Something</p></div>')		
+ 					html.append('<div id="text-bucket-styled" class="reader"><p>Im looking at</p></div>')	
+ 					html.append('<div id="text-bucket-end" class="downer"><p>Something dude was almost walking...</p></div>')	
+ 	  				html.append('</div>')
+	  var from = $("#text-bucket-end",html);
+	  var to = $("#text-bucket-styled",html);
+	  reader.redistributing_up(from,to,3)
+    expect("Im looking at Something dude was").toEqual(to.text());
+    expect("almost walking...").toEqual(from.text());
+	
+  });
+  it("10 should be able to redistribute from bottom to reader and erase bottom paragraph if empty", function() {
+ var html = $('<div class="book">')		
+	  				html.append('<div id="upper-container" class="upper"><p>Something</p></div>')		
+ 					html.append('<div id="text-bucket-styled" class="reader"><p>Im looking at</p></div>')	
+ 					html.append('<div id="text-bucket-end" class="downer"><p>Something dude was </p></div>')	
+ 	  				html.append('</div>')
+	  var from = $("#text-bucket-end",html);
+	  var to = $("#text-bucket-styled",html);
+	  reader.redistributing_up(from,to,3)
+    expect("Im looking at Something dude was").toEqual(to.text());
+    expect(0).toEqual(from.children().length);
+	
+  });
+  
+  it("11 should be able to redistribute from reader to bottom to bottom erase bottom paragraph if empty", function() {
+ var html = $('<div class="book">')		
+	  				html.append('<div id="upper-container" class="upper"><p>Something</p></div>')		
+ 					html.append('<div id="text-bucket-styled" class="reader"><p>Im looking at</p></div>')	
+ 					html.append('<div id="text-bucket-end" class="downer"><p>Something dude was </p></div>')	
+ 	  				html.append('</div>')
+	  var from = $("#text-bucket-styled",html);
+	  var to = $("#text-bucket-end",html);
+	  reader.redistributing_down(from,to,3)
+    expect("Im looking at Something dude was ").toEqual(to.text());
+    expect(0).toEqual(from.children().length);
+	
+  });
+
+  it("12 should be able to redistribute from reader to upper creating new paragraph because carriage", function() {
+ var html = $('<div class="book">')		
+	  				html.append('<div id="upper-container" class="upper"><p>Something</p></div>')		
+ 					html.append('<div id="text-bucket-styled" class="reader"><p>Im looking \n</p></div>')	
+ 					html.append('<div id="text-bucket-end" class="downer"><p>Something dude was </p></div>')	
+ 	  				html.append('</div>')
+	  var from = $("#text-bucket-styled",html);
+	  var to = $("#upper-container",html);
+	  reader.redistributing_up(from,to,3)
+    expect("Something Im looking \n").toEqual(to.children().first().text());
+    expect("").toEqual(to.children().last().text());	
+  });
+  
+});
--- EyeShake/js/fastReader.js
+++ EyeShake/js/fastReader.js
@@ -1,257 +1,76 @@
-var items = ['Im very happy'];
-var width = 600;
-var time_to_read = 100;
-var text = 'In the twelfth and thirteenth centuries, the Jurchen Jin Dynasty (1115–1234) waged a series of military campaigns against the Chinese Song Dynasty (960–1279). In 1115, the Jurchens rebelled against their overlords, the Khitans of the Liao Dynasty (907–1125), and declared the formation of the Jin. Allying with the Song against their common enemy the Liao, the Jin promised to return to the Song the territories in northern China that had fallen under Liao control since 938. The Jurchens quick defeat of the Liao combined with Song military failures made the Jin reluctant to cede these territories. After a series of failed negotiations that embittered both sides, the Jurchens attacked the Song in November 1125, dispatching one army towards Taiyuan and the other towards Kaifeng, the Song capital. Surprised by the news of an invasion, the Song general stationed in Taiyuan retreated from the city, which was besieged and later captured. As the second Jin army approached the capital, Emperor Huizong of the Song abdicated and fled south. A new emperor, Qinzong, was enthroned. The Jurchens began a siege against Kaifeng in 1126, but Qinzong negotiated for their retreat from the capital after he agreed to pay a large annual indemnity. Qinzong reneged on the deal and ordered Song forces to defend the prefectures instead of fortifying the capital. The Jin resumed their war against the Song and again besieged Kaifeng in 1127. The Chinese emperor was captured in an event known as the Jingkang Incident, the capital was looted, and the Song lost northern China to the Jin. Remnants of the Song retreated to southern China and, after brief stays in several temporary capitals, eventually relocated to Hangzhou. The retreat of the Song court marked the end of the Northern Song era and the beginning of the Southern Song.'
-//var text = 'Lorem ipsum dolor draw attention sit amet, consectetuer adipiscing elit. Nam nibh. Nunc varius facilisis eros';
-var number_words = 3;
-var words_to_watch;
-var response;
-var min_total_letters_factor = 4;
 
 
 
-
-function modify_width(to_add){
-	this.width = this.width + to_add;
-	apply_width();
-}
-
-function modify_words(to_add){
-	this.number_words = this.number_words + to_add;
-	apply_number_words();
-}
-
-function modify_time_to_read(to_add){
-	this.time_to_read = this.time_to_read + to_add;
-	apply_time_to_read();
-}
-
-function apply_time_to_read(){
-	$("#time_to_read").text(this.time_to_read/1000)
-}
-
-function apply_number_words(){
-	$("#number_words").text(this.number_words);
-	
-}
-
-function apply_width(){
-	$("#reader").css("width",this.width)
-	
-	
-	
+function FastReader() {
 }
 
 
-function add_width(){
-modify_width(50)
-}
-
-function shrink_width(){
-	modify_width(-50)
-}
-
-var lastTopPar = "";
-var lastBottomPar = "";
-
-
-
-function build_div(pos){	
-	var words = get_words(text,number_words,pos); 
-	words_to_watch = print_words(words[0]);
-	var index = text.indexOf(words_to_watch)
-	//$("#text").text(words_to_watch);
-	lastTopPar = append_p_to_div(this.text.substring(index,  index + words_to_watch.length),"#upper-container",lastTopPar,true)
-	printPart(this.text,0,index,"text-bucket-init")
-	printPart(this.text,index,index + words_to_watch.length,"text-bucket-styled")
-	update_bottom(words_to_watch)
-	return words[1];
-}
-
-function build_div2(paragraphId,number_words){
-	
-}
-
-function append_p_to_div(words,div,lastPar,up){
-	if(words.indexOf("\n")>-1){
-		$(div).append("<p>"+lastPar + words.substring(0,words.indexOf("\n"))+"</p>")
-		var dif = $(div)
-		if(up){dif.scrollTop(dif[0].scrollHeight)}else{};
-		return words.substring(words.indexOf("\n"),words.length)
-	}
-	return lastPar + words
-}
-
-function populate_bottom(text) {
-	$.each(text.split("\n"),function( index,paragraph ) {
-		$("#text-bucket-end").append("<p>"+paragraph+"</p>")
-	})
-}
-function build_paragraph(text,div,order){
-	var content = $(div+" p").apply(order).text()
-	if(text.indexOf("\n")>-1){
-		$(div+ " p").apply(order).remove()		
+FastReader.prototype.take_text = function(paragraph,amount,begining) {
+	if(begining){
+		return paragraph.substring(0,this.indexOfRec(paragraph,amount," ")+1)
 	}else{
-		$(div+ " p").apply(order).remove()
-		$(div).append("<p>"+content.substring(content.indexOf(text)+text.length)+"</p>")		
-	}	
-}
+		//if not begining then end is supossed
+		return paragraph.substring(this.lastIndexOfRec(paragraph,amount," ")-1)	
+	}
+};
 
-function take_from_text(paragraph,amount){
-	paragraph.substring(paragraph.indexOf(amount))
-}
-
-function take_from_paragraph(div,number_words){
-	
-	var content = $(div+" p").apply(order).text()
-	if(text.indexOf("\n")>-1){
-		$(div+ " p").apply(order).remove()		
+FastReader.prototype.delete_text = function(paragraph,amount,begining) {
+	if(begining){
+		return paragraph.substring(this.indexOfRec(paragraph,amount," ")+1)
 	}else{
-		$(div+ " p").apply(order).remove()
-		$(div).append("<p>"+content.substring(content.indexOf(text)+text.length)+"</p>")		
-	}	
+		//if not begining then end is supossed
+		return paragraph.substring(0,this.lastIndexOfRec(paragraph,amount," ")-1)	
+	}
+};
+
+FastReader.prototype.move_text = function(from,to,amount,fromBeginingFrom,toBeginningTo){		
+	if(toBeginningTo) return this.take_text(from,amount,fromBeginingFrom).trim()+" "+to
+		else return to+" "+this.take_text(from,amount,fromBeginingFrom).trim()
+			
+    
 }
 
-
-
-//not need a function for this
-function redistribute_paragraph(paragraph,amount,dest1,dest2,dest3){
-	dest3(dest2(dest1(paragraph,text)))
+FastReader.prototype.indexOfRec = function(paragraph,amount,toFind){
+	return paragraph.split(toFind, amount).join(toFind).length
 }
-
-function update_bottom(text) {
-	var content = $("#text-bucket-end p").first().text()
-	if(text.indexOf("\n")>-1){
-		$("#text-bucket-end p").first().remove()
-		
+FastReader.prototype.lastIndexOfRec = function(paragraph,amount,toFind){
+	var reversed = paragraph.split("").reverse().join("");
+	return paragraph.length - this.indexOfRec(reversed,amount,toFind)
+}
+FastReader.prototype.redistributing_up = function (from,to,amount){	
+	//create new objects!!!! make it functional
+	var toUpdated = this.move_text(from.text(),to.children().last().text(),amount,true,false)	
+	from.text(this.delete_text(from.text(),amount,true))	
+	if(to.children().length == 0){
+		to.append('<p>'+toUpdated.trim()+'</p>')
+	}else if(toUpdated.indexOf("\n")==-1){		
+		to.children().last().text(toUpdated)
 	}else{
-		$("#text-bucket-end p").first().remove()
-		$("#text-bucket-end").prepend("<p>"+content.substring(content.indexOf(text)+text.length)+"</p>")		
+		//contains carriage
+		to.children().last().text(toUpdated.substring(toUpdated.indexOf("\n")))		
+		to.append('<p>'+toUpdated.substring(0,toUpdated.indexOf("\n")).trim()+'</p>')
 	}
 }
 
-
-
-var currentPos = 0;
-function blink(){
-	currentPos = build_div(this.currentPos);
-	setTimeout(blink,time_to_read);
-}
-
-function start_horizontal() {
-		setInterval(blink,1000);	
-}
-
-function get_words(text,number_words,pos){
-	var words =  text.split(' ');
-	var chunk = new Array();
-	var words_sin = 0;
-	for(var i=0;words_sin<number_words;i++){
-		chunk[i] = words[pos+i];
-		words_sin = words_sin + 1
-		
+FastReader.prototype.redistributing_down = function (from,to,amount){	
+	//create new objects!!!! make it functional	
+	var toUpdated = this.move_text(from.text(),to.children().first().text(),amount,false,true)	
+	from.text(this.delete_text(from.text(),amount,false))		
+	if(to.children().length == 0){
+		to.prepend('<p>'+toUpdated.trim()+'</p>')
+	}else if(toUpdated.indexOf("\n")==-1){		
+		to.children().first().text(toUpdated)
+	}else{
+		//contains carriage
+		to.children().first().text(toUpdated.substring(toUpdated.indexOf("\n")))		
+		to.prepend('<p>'+toUpdated.substring(0,toUpdated.indexOf("\n")).trim()+'</p>')
 	}
-	return [chunk,pos+chunk.length];
 }
 
-function printPart(initText,from,to,where){
-	$("#"+where).text(initText.substring(from,to))
-}
+//make blink reader on punto
+//make blink upper on punto y aparte
 
 
-
-
-function print_words(words){
-	var to_print = '';
-	for(var i=0;i<words.length;i++){
-		to_print = to_print + words[i]+' '
-	}
-	return to_print;
-}
-
-
-
-
-
-
-
-$(document).ready(function() {
-	$("#start").click(blink);
-	//configuration buttons
-	$("#more_width").click(add_width);
-	$("#less_width").click(shrink_width);
-	$("#more_words").bind("click",function(e){
-		modify_words(1)
-	});
-	$("#less_words").bind("click",function(e){
-		modify_words(-1)
-	});
-	$("#less_time_to_read").bind("click",function(e){
-		modify_time_to_read(-1000)
-	});
-	$("#more_time_to_read").bind("click",function(e){
-		modify_time_to_read(1000)
-	});
-	//avoiding reload page
-	$("#response").bind("enterKey",function(e){
-		blink();
-		});
-	$("#response").keydown(function(e){
-	    if(e.keyCode == 13)
-	    {
-	        $(this).trigger("enterKey");
-	    }
-	});
-	
-	$("#main_form").submit(function(event) {
-	      
-		  /* stop form from submitting normally */
-	      event.preventDefault();
-	       $("#upload-button").innerHTML = 'Uploading...';
-	       // Get the selected files from the input.
-	        var files = $("#file-select")[0].files;
-	       // Create a new FormData object.
-	        var formData = new FormData();
-	       // Loop through each of the selected files.
-	        for (var i = 0; i < files.length; i++) {
-	          var file = files[i];
-	       // Check the file type.
-	       // Add the file to the request.
-	        fr = new FileReader();
-	        fr.onload = receivedText;
-	       fr.readAsText(file);
-	       //                   fr.readAsDataURL(file);
-	          formData.append('photos[]', file, file.name);
-		}
-	  });
-	  
-	  $("#text-bucket").change(function(e) {
-	    //this.text = $("#text-bucket").val();
-		assign_to_bucket($("#text-bucket").val());
-		//build_div();
-	  });
-	//$("#text-bucket").text(text);
-	apply_width();
-	apply_number_words();
-	apply_time_to_read();
-});
-
-
-function assign_to_bucket(text) {
-	this.text = text;
-}
-
-function receivedText() {
-	  //document.getElementById('editor').appendChild(document.createTextNode(fr.result));
-	assign_to_bucket(fr.result);
-	populate_bottom(fr.result)
-	  
-	      }    
-
-
-var form = document.getElementById('main_form');
-var fileSelect = document.getElementById('file-select');
-var uploadButton = document.getElementById('upload-button');
 
 
 
